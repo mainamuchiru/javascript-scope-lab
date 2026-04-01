ec77e5d67d4b82c984db8108d5967336e4352f23
mainamuchiru <mainaephantus@gmail.com>
2026-04-01 11:58:39 +0300

    Changed code block

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let featuredDrink = "Strawberry Milkshake";
 function addBurger() {
   const newBurger = "Flatburger";
   burgers.push(newBurger);
-  if (true) {
-    const anotherNewBurger = "Maple Bacon Burger";
-    burgers.push(anotherNewBurger);
+}
+
+if (true) {
+  const anotherNewBurger = "Maple Bacon Burger";
+  burgers.push(anotherNewBurger);
+  function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";
   }
 }
-function changeFeaturedDrink() {
-  featuredDrink = "The JavaShake";
-}
-changeFeaturedDrink()
